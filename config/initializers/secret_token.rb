b997f04e7c30de29038241a6c90f676d61cb3928
Pablo Villalba <pablo@teambox.com>
2010-11-12 12:54:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cocina::Application.config.secret_token = '60c95124617dd6bf7d7bcf2907eff684411454ba27585a7c4edf4443c80508a626f41fccdbe236f6dc56881a1991e3624356102ddbe8b3b608dea65ba2d4da5d'
